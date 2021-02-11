5eaa9ccd55ad12e31970eda840704cf4de7114d6
Haneul Jin <77265562+knsan189@users.noreply.github.com>
2021-02-11 17:05:26 +0900

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,48 @@
+
+
+window.addEventListener('DOMContentLoaded', function(){
+    
+    let megaMenu = document.querySelector('.depth1 .menu')
+    let depth2 = document.querySelector('.depth2')
+
+    megaMenu.addEventListener('mouseover', function(){
+        depth2.classList.add('on')
+    })
+    megaMenu.addEventListener('mouseout', function(){
+        depth2.classList.remove('on')
+    })
+
+
+
+
+
+    let tabbarLi = document.querySelectorAll('.tabbar > li')
+    for(let i=0;i<tabbarLi.length;i++){
+        tabbarLi[i].addEventListener('click', function(){
+            this.classList.add('active')
+            // this.children[1].style.display = "block"
+            for(let siblings of this.parentNode.children){
+                if(siblings !== this){
+                    siblings.classList.remove('active')
+                    // siblings.children[1].style.display = "none"
+                }
+            }
+        })
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
